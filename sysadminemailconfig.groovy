656ab78427a924e792db51dbf20b21ba08bcdf12
sgee <sarveshhh@gmail.com>
2017-12-24 19:19:33 +0530

    Update sysadminemailconfig.groovy

--- sysadminemailconfig.groovy
+++ sysadminemailconfig.groovy
@@ -1,6 +1,5 @@
 #!Groovy
 //This groovy script will configure the email address for admin
-//Created By: Sarvesh Gupta/Himanshu Goel
 
 import jenkins.model.*
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
